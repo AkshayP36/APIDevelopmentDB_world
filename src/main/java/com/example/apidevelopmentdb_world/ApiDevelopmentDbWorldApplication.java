f04c5845441a4763a8f5a420114b5390c1369704
Akshay Patil <studyakshaystudy@outlook.com>
2025-02-02 00:54:48 +0800

    first commit

---
+++ src/main/java/com/example/apidevelopmentdb_world/ApiDevelopmentDbWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidevelopmentdb_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDevelopmentDbWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDevelopmentDbWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apidevelopmentdb_world/ApiDevelopmentDbWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidevelopmentdb_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopmentDbWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
